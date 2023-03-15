6c471fda999532a26f45ec5f923983be90768ab1
koneneru <imarikkard@gmail.com>
2023-03-16 03:02:17 +0400

    Добавление PowerCollections

---
+++ PowerCollections/PowerCollections.Tests/TestStack.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Reflection;
+using Wintellect.PowerCollections;
+
+namespace PowerCollections.Tests
+{
+    [TestClass]
+    public class StackTests
+    {        
+        //Constructor test
+        [TestMethod]
+        public void TestConstructorStack()
+        {
+            Stack<int> stack = new Stack<int>(10);
+            Assert.AreEqual(0, stack.Count);
+            Assert.AreEqual(10, stack.Capacity);
+        }
+
+        //Enumerator Test
+        [TestMethod]
+        public void Enumerator_ReturnInvertedStack()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            int[] Arr = new int[] { 3, 2, 1 };
+            int[] testArr = new int[3];
+            int i = 0;
+            foreach (int item in stack)
+                testArr[i++] = item;
+            CollectionAssert.AreEqual(Arr, testArr);
+        }
+
+        //Push tests
+        [TestMethod]
+        public void TestPush_IfStackNotIsFull()
+        {
+            Stack<string> stack = new Stack<string>(10);
+            stack.Push("1");
+            stack.Push("2");
+            stack.Push("3");
+            Assert.AreEqual(3, stack.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestPushWithException_IfStackFull()
+        {
+            Stack<string> stack = new Stack<string>(1);
+            stack.Push("1");
+            stack.Push("2");
+        }
+
+        //Pop tests
+        [TestMethod]
+        public void TestPop_IfStackNotIsEmpty()
+        {
+            Stack<string> stack = new Stack<string>(10);
+            string a = "1";
+            string b = "2";
+            stack.Push(a);
+            stack.Push(b);
+            string pop1 = stack.Pop();
+            string pop2 = stack.Pop();
+            Assert.AreEqual(pop1, b);
+            Assert.AreEqual(pop2, a);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestPopWithException_StackIsEmpty()
+        {
+            Stack<int> stack = new Stack<int>(1);
+            stack.Pop();
+        }
+
+        //Top Tests
+        [TestMethod]
+        public void TestTop_IfStackNotIsEmpty()
+        {
+            Stack<string> stack = new Stack<string>(10);
+            stack.Push("1");
+            stack.Push("2");
+            stack.Push("3");
+            string t1 = stack.Top();
+            string t2 = stack.Top();
+            Assert.AreEqual("3", t1);
+            Assert.AreEqual("3", t2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void TestTopWithException_StackIsEmpty()
+        {
+            Stack<int> stack = new Stack<int>(1);
+            stack.Top();
+        }
+
+        //IsEmpty Tests
+        [TestMethod]
+        public void TestIsEmpty_TrueWhenCreateNewStack()
+        {
+            Stack<string> stack = new Stack<string>(10);
+            Assert.IsTrue(stack.IsEmpty);
+        }
+
+        [TestMethod]
+        public void TestIsEmpty_FalseAfterPush()
+        {
+            Stack<int> stack = new Stack<int>(10);
+            stack.Push(1);
+            Assert.IsFalse(stack.IsEmpty);
+        }
+
+        [TestMethod]
+        public void TestIsEmpty_TrueAfterPop()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Push(1);
+            stack.Pop();
+            Assert.IsTrue(stack.IsEmpty);
+        }
+
+
+        //IsFull tests
+        [TestMethod]
+        public void TestIsFull_TrueAfterFilligStack()
+        {
+            Stack<string> stack = new Stack<string>(1);
+            stack.Push("r");
+            Assert.IsTrue(stack.IsFull);
+        }
+
+        [TestMethod]
+        public void TestIsFull_TrueWhenCreateNewStack()
+        {
+            Stack<int> stack = new Stack<int>(10);
+            Assert.IsFalse(stack.IsFull);
+        }
+
+        [TestMethod]
+        public void TestIsFull_FalseAfterPop()
+        {
+            Stack<int> stack = new Stack<int>(2);
+            stack.Push(2);
+            stack.Push(1);
+            stack.Pop();
+            Assert.IsFalse(stack.IsFull);
+        }
+
+        //Exception
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void TestException_WhenCreateNewStackWithNegativeValue()
+        {
+            Stack<int> stack = new Stack<int>(-2);
+        }
+    }
+}
---
+++ PowerCollections/PowerCollections/Stack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Wintellect.PowerCollections
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        public int Capacity;//размер стека
+        private T[] Items; // элементы стека
+        private int count;  // количество элементов
+        //Размер стека
+        public int Count
+        {
+            get { return count; }
+        }
+
+        //Constructor
+        public Stack(int size = 100)
+        {
+            Items = new T[size];
+            Capacity = size;
+            count = 0;
+        }
+        //Enumerator
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count; i > 0; i--)
+            {
+                yield return Items[i - 1];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        //Проверка на заполненость
+        public bool IsEmpty
+        {
+            get { return count == 0; }
+        }
+        public bool IsFull
+        {
+            get { return Count == Capacity; }
+        }
+
+        //Добавление элемента
+        public void Push(T item)
+        {
+            Items[count++] = item;
+        }
+        //Снимаю элемент с вершины и возвращаю его значение
+        public T Pop()
+        {
+            return Items[--count];
+        }
+        //Возвращаю значение элемента на вершине стека, но не извлекаю
+        public T Top()
+        {
+            return Items[count - 1];
+        }
+    }
+}
